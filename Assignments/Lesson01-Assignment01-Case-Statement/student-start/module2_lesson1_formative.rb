18bbcb52eb876be58b0db31d6f96321841381f42
SaiKUndurthi <sai.undurthi@gmail.com>
2016-06-25 20:38:53 -0700

    Implemented Case instead of if-else statements.

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,13 @@
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
+case some_var
+when "pink elephant"
   puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
+when false
   puts "Looks like this one should execute"
-
+when another_var.nil?
+  puts "Question mark in the method name?"
 else
   puts "I guess nothing matched... But why?"
 end
